224213496219c57f07b14882bbf5a9c94cdf5d08
Your Name <you@example.com>
2023-09-15 14:59:36 +0700

    luon qua

--- src/tuan2/BaiTH/CDList.java
+++ src/tuan2/BaiTH/CDList.java
@@ -1,5 +1,24 @@
 package tuan2.BaiTH;
 
 public class CDList {
-	public static void 
+	private CD[] list;
+	private int n = 0;
+	
+	
+	public CD[] getList() {
+		return list;
+	}
+	public void setList(CD[] list) {
+		this.list = list;
+	}
+	public int getN() {
+		return n;
+	}
+	public void setN(int n) {
+		this.n = n;
+	}
+	
+	
+	
+	
 }
--- src/tuan2/BaiTH/TestCD.java
+++ src/tuan2/BaiTH/TestCD.java
@@ -1,8 +1,18 @@
 package tuan2.BaiTH;
 
 public class TestCD {
-	public static void main (String[] args) {
-		CD cd1 = new CD(8888, "pine", 9, 86.86);
-		System.out.println(cd1);
-	}
+    public static void main(String[] args) {
+        // Declare and create an array to store CD objects
+        CD[] cdArray = new CD[3]; // You can change the size as needed
+
+        // Create CD objects and add them to the array
+        cdArray[0] = new CD(8888, "Album 1", 9, 86.86);
+        cdArray[1] = new CD(7777, "Album 2", 12, 105.50);
+        cdArray[2] = new CD(6666, "Album 3", 10, 92.75);
+
+        // Print the information about each CD in the array
+        for (CD cd : cdArray) {
+            System.out.println(cd);
+        }
+    }
 }
